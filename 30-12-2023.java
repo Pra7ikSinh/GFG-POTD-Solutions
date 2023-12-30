05f4e4513174e97fbebdc1071457caac5c587f32
Pra7iksinh <pra7iksinh@gmail.com>
2023-12-30 18:19:25 +0530

    GFG POTD - 30/12/2023 Problem solution

---
+++ 30-12-2023.java
@@ -0,0 +1,35 @@
+/*******************************************************************
+
+Author : Pra7iksinh Parmar
+Date : 30/12/2023
+Problem : Winner of an election
+Problem Link : https://www.geeksforgeeks.org/problems/winner-of-an-election-where-votes-are-represented-as-candidate-names-1587115621/1
+
+************************************************************************/
+
+class Solution
+{
+    //Function to return the name of candidate that received maximum votes.
+    public static String[] winner(String arr[], int n)
+    {
+        // add your code
+        HashMap<String,Integer> hm = new HashMap<>();
+        String res[] = new String[2];
+        int max = 0;
+        for(int i=0; i<n; i++){
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+            max = Math.max(hm.get(arr[i]),max);
+        }
+        
+        for(String key : hm.keySet()){
+            if(hm.get(key)==max){
+                if(res[0]==null) res[0]=key;
+                else{
+                    if(res[0].compareTo(key)>0) res[0]=key;
+                }
+            }
+        }
+        res[1] = String.valueOf(max);
+        return res;
+    }
+}
