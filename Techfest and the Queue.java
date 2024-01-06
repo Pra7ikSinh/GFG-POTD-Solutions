cfcaef12e235e894bae691ab34d3d5dbd4687cce
Pra7iksinh <pra7iksinh@gmail.com>
2024-01-06 20:59:14 +0530

    GFG POTD - Techfest and the Queue - 06/01/2023 Problem solution

---
+++ Techfest and the Queue.java
@@ -0,0 +1,28 @@
+/*******************************************************************
+
+Author : Pra7iksinh Parmar
+Date : 06/01/2024
+Problem : Techfest and the Queue
+Problem Link : https://www.geeksforgeeks.org/problems/techfest-and-the-queue1044/1
+
+************************************************************************/
+
+class Solution {
+    public static long sumOfPowers(long a, long b) {
+        // code here
+        long sum = 0;
+        
+        for(int i=(int)a; i<=(int)b; i++){
+            int x = i;
+            for(int j=2; j*j<=x; j++){
+                while(x%j==0){
+                    sum++;
+                    x=x/j;
+                }
+            }
+            if(x>=2) sum++;
+        }
+        return sum;
+    }
+}
+  
