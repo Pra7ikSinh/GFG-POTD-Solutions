77fee335f5c88495e84028787730eae1683ec69c
Pra7iksinh <pra7iksinh@gmail.com>
2024-02-18 16:49:40 +0530

    Problem solution of 18-02-2024

--- 18-02-2024.cpp
+++ 18-02-2024.cpp
@@ -7,3 +7,30 @@ Problem Link : https://www.geeksforgeeks.org/problems/find-element-occuring-once
 
 ************************************************************************************************************************************************************************/
 
+
+class Solution
+{
+    public:
+        int sum=0;
+        
+        void traversal(Node* root){
+            if(root==NULL) return;
+            if(root->left==NULL && root->right==NULL){
+                sum+=root->data;
+                return;
+            }
+            
+            traversal(root->right);
+            traversal(root->left);
+            
+            return ;
+            
+        }
+        
+        /*You are required to complete below method */
+        int sumOfLeafNodes(Node *root ){
+            /*Your code here */
+            traversal(root);
+            return sum;
+        }
+};
